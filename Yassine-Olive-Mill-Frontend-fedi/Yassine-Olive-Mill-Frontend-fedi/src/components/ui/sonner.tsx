eea17c947db7abb9134d3cbb170322d0a7d73fe7
fedibbm <benbrahimfedi4@gmail.com>
2025-10-12 12:54:18 +0100

    toasts are now rtl aware

--- Yassine-Olive-Mill-Frontend-fedi/Yassine-Olive-Mill-Frontend-fedi/src/components/ui/sonner.tsx
+++ Yassine-Olive-Mill-Frontend-fedi/Yassine-Olive-Mill-Frontend-fedi/src/components/ui/sonner.tsx
@@ -1,15 +1,19 @@
 import { useTheme } from "next-themes";
+import { useTranslation } from "react-i18next";
 import { Toaster as Sonner, toast } from "sonner";
 
 type ToasterProps = React.ComponentProps<typeof Sonner>;
 
 const Toaster = ({ ...props }: ToasterProps) => {
   const { theme = "system" } = useTheme();
+  const { i18n } = useTranslation();
+  const isRTL = i18n.language === 'ar';
 
   return (
     <Sonner
       theme={theme as ToasterProps["theme"]}
       className="toaster group"
+      position={isRTL ? "bottom-left" : "bottom-right"}
       toastOptions={{
         classNames: {
           toast:
