04a2d5a4b7d753594db42a1adb99e7de81d69e23
fedibbm <benbrahimfedi4@gmail.com>
2025-11-05 22:11:08 +0100

    changed order of tabs

--- Yassine-Olive-Mill-Frontend-fedi/Yassine-Olive-Mill-Frontend-fedi/src/components/layout/MainLayout.tsx
+++ Yassine-Olive-Mill-Frontend-fedi/Yassine-Olive-Mill-Frontend-fedi/src/components/layout/MainLayout.tsx
@@ -30,7 +30,6 @@ const navigationItems = [
   { key: 'dashboard', icon: LayoutDashboard, href: '/dashboard', roles: ['admin'] },
   { key: 'clients', icon: Users, href: '/clients', roles: ['admin'] },
   { key: 'tickets', icon: FileText, href: '/tickets', roles: ['admin'] },
-  { key: 'payments', icon: DollarSign, href: '/payments', roles: ['admin'] },
 
   { key: 'containers', icon: Container, href: '/containers', roles: ['admin', 'operator'] },
   { key: 'scanner', icon: QrCode, href: '/scanner', roles: [ 'scanner'] },
@@ -39,6 +38,7 @@ const navigationItems = [
   { key: 'employeeScanner', icon: Building2, href: '/employee-scanner', roles: ['operator'] },
   // { key: 'batchManagement', icon: ClipboardList, href: '/batch-management', roles: ['admin', 'operator', 'presser'] },
   { key: 'pressingDisplay', icon: Monitor, href: '/pressing-display', roles: ['admin'] },
+  { key: 'payments', icon: DollarSign, href: '/payments', roles: ['admin'] },
   { key: 'rooms', icon: Building2, href: '/rooms', roles: ['admin', 'operator'] },
   { key: 'userManagement', icon: Users, href: '/admin/users', roles: ['admin'] },
   { key: 'settings', icon: Settings, href: '/settings', roles: ['admin'] },
